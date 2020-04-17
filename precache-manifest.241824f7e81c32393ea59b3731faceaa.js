ccaa6324bbee825b3e4f0b68cd07acaa30675aef
lombocska <lombos.monika@gmail.com>
2020-04-17 17:56:58 +0200

    Updates

---
+++ precache-manifest.241824f7e81c32393ea59b3731faceaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a15ed25fd9d8d615a5185898aa33e49",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "32ee3cffd3fe5dd507ce",
+    "url": "/burger-builder/static/css/main.2d122462.chunk.css"
+  },
+  {
+    "revision": "656de96944a340a2768c",
+    "url": "/burger-builder/static/js/2.ae11d421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.ae11d421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32ee3cffd3fe5dd507ce",
+    "url": "/burger-builder/static/js/main.c96a8818.chunk.js"
+  },
+  {
+    "revision": "aa51b73ea7754a5431dd",
+    "url": "/burger-builder/static/js/runtime-main.27f48eea.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);

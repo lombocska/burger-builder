94cad55d13e960d58f784f223a145908106c772a
lombocska <lombos.monika@gmail.com>
2020-04-17 17:41:13 +0200

    feat: creating burger, checkout, mobile-first lookup

---
+++ src/components/Navigation/SideDrawer/DrawerToggler/DrawerToggler.js
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './DrawerToggler.module.css';
+
+const drawerToggle = ( props ) => (
+    <div
+        className={classes.DrawerToggle}
+        onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default drawerToggle;
